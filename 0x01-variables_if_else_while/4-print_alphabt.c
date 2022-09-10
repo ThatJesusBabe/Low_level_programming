0c2a68b0d97ad8a1e933c623ebfc48f219541393
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-09 19:14:07 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int ch;
-
+char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 if (ch != 'e' && ch != 'q')
 {
 putchar(ch);	
-}
-else {
 }
 }
 putchar('\n');
