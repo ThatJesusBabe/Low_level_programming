df012c5c6a99f16a330de0791d1f5116eeeb4996
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-09 18:18:09 -0700

    commiting changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 	int c, i, k, j;
 
 	for (c = 48; c <= 57; c++)
-	{
+	{ 
 		for (i = 48; i <= 57; i++)
-		{
+	       	{
 			for (k = 48; k <= 57; k++)
 			{
 				for (j = 48; j <= 57; j++)
