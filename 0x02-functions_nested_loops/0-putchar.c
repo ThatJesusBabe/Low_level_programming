fe4dec88470cd8e707fba493079ffd85940bbc3c
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-13 13:37:51 -0700

    commiting changes

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+* Description: prints _putchar followed by a newline
+* Return: Zero;
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file is about everything about nested loop.
