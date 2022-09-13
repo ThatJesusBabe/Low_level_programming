3fd8771a97ad78d71a74f85ae8d1f4312ecc76a8
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-13 16:22:26 -0700

    commit newww

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,15 @@
 */
 int main(void)
 {
-	_putcher('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+	{
+		_putchar(str[ch]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
 
