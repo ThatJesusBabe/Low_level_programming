f28860b04ea4b30591985c1ad8022d88376cb6da
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-13 18:17:52 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
-* main - check the code
 * print_alphabet - print alphabeth.
 * Return: Always 0.
 */
@@ -12,7 +11,8 @@ void print_alphabet(void)
 char ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
+{
 _putchar(ch);
 _putchar('\n');
-
+}
 }
