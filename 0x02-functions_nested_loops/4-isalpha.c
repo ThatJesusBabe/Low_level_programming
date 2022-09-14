62fc76840a58965182715f6f28c25371251ef3bb
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-14 14:16:36 -0700

    commiting changes to task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,16 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&  c >= 65 && c >= 90)
-		return (1);
-	return (0);
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
