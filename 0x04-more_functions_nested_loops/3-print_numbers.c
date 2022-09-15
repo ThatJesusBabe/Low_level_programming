0c7aa880f5c75c7a0a2e779c1fd1676c06f7d872
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-15 13:35:52 -0700

    pushing edited task 3 and 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print number 0 to 9
+* Return : Always 0
+*/
+
+void print_most_numbers(void)
+{
+	int num1;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		_putchar(num1);
+		_putcher('\n');
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print number 0 to 9
+* Return : Always 0
+*/
+
+void print_most_numbers(void)
+{
+	int num1;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		if (num1 != 2  && num1 != 4)
+		{
+			_putchar(num1);
+			_putcher('\n');
+		}
+	}
+	return (0);
+}
