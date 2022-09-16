064abf3697572dbe4234ec5480d8e5bc83144f1e
Lorrine Adefemi <adefemilauren@gmail.com>
2022-09-15 18:18:52 -0700

    push changes to task 4 to 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+
+{
+
+	int i;
+
+	for (i = 0; i < n; ++i)
+	_putchar('_');
+	_putchar('\n');
+
+	return;
+
+}
