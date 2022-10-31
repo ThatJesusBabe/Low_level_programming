4d443237c1ba7664fcd915ba5d4d85f950ce68b4
Lorrine Adefemi <adefemilauren@gmail.com>
2022-10-31 16:28:27 -0700

    commit changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,8 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnt1.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
